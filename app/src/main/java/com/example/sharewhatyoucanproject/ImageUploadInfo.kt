d27dcc2528ac150c8fd716dbb9ee153f2cafc18a
Amulya <88301130+gangamamulya@users.noreply.github.com>
2022-06-04 10:19:15 -0700

    Changes made to image upload info file

--- app/src/main/java/com/example/sharewhatyoucanproject/ImageUploadInfo.kt
+++ app/src/main/java/com/example/sharewhatyoucanproject/ImageUploadInfo.kt
@@ -37,18 +37,11 @@ import android.os.IBinder
 class ImageUploadInfo {
     var imageName: String? = null
     var imageURL: String? = null
-
-    /*public float getLocationButton() {
-         return locationButton;
-     }*/
     var imageDescription: String? = null
-
-    //public float locationButton;
     constructor() {}
     constructor(name: String?, url: String?, descr: String?) {
         imageName = name
         imageURL = url
         imageDescription = descr
-        //this.locationButton = loc;
     }
 }
